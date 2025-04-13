02ef883a203201ef85be261435b64d83196906c9
Anurag Mittal <1321012+anurag4DSB@users.noreply.github.com>
2025-04-13 16:25:50 +0200

    Remove namespace flag and check for CSI driver pods across all namespaces

--- tests/e2e-scality/e2e-tests/csi_driver_test.go
+++ tests/e2e-scality/e2e-tests/csi_driver_test.go
@@ -41,19 +41,12 @@ package main
 
 import (
 	"context"
-	"flag"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-)
-
-// Command-line flags
-var (
-	// namespaceFlag overrides the namespace used in tests
-	namespaceFlag = flag.String("namespace", "mount-s3", "Namespace where CSI driver is installed")
 )
 
 // TestScalityCSIDriver is the main Go test function that triggers the Ginkgo framework
@@ -69,40 +62,35 @@ func TestScalityCSIDriver(t *testing.T) {
 // go test -v -tags=e2e -ginkgo.focus="Basic" -ginkgo.skip="Volume"
 
 var _ = Describe("Scality S3 CSI Driver", func() {
-	// Define test namespace for resources
-	var (
-		driverNamespace string
-	)
-
-	BeforeEach(func() {
-		// Get namespace from flag
-		driverNamespace = *namespaceFlag
-		GinkgoWriter.Write([]byte("Using namespace: " + driverNamespace + "\n"))
-	})
-
 	// Test basic driver functionality
 	Describe("Basic Functionality", func() {
-		It("should have CSI driver pods running", func() {
-			By("Checking CSI driver pods in " + driverNamespace + " namespace")
-			pods, err := clientset.CoreV1().Pods(driverNamespace).List(context.Background(), metav1.ListOptions{})
-			Expect(err).NotTo(HaveOccurred(), "Failed to list pods in %s namespace", driverNamespace)
-
-			// Check that at least one pod exists
-			Expect(pods.Items).NotTo(BeEmpty(), "No CSI driver pods found in %s namespace", driverNamespace)
-
-			// Check that all pods are running
-			for _, pod := range pods.Items {
-				Expect(pod.Status.Phase).To(Equal(corev1.PodRunning),
-					"Pod %s in %s namespace is not in Running state", pod.Name, driverNamespace)
-			}
-		})
-
 		It("should have CSI driver properly registered", func() {
 			By("Checking for CSI driver registration")
 			driver, err := clientset.StorageV1().CSIDrivers().Get(context.Background(),
 				"s3.csi.aws.com", metav1.GetOptions{})
 			Expect(err).NotTo(HaveOccurred(), "Failed to get CSI driver s3.csi.aws.com")
 			Expect(driver).NotTo(BeNil(), "CSI driver s3.csi.aws.com not found")
+		})
+
+		It("should have CSI driver pods running", func() {
+			By("Checking CSI driver pods in all namespaces")
+			pods, err := clientset.CoreV1().Pods(corev1.NamespaceAll).List(context.Background(), metav1.ListOptions{
+				LabelSelector: "app in (csi-driver,s3-csi)",
+			})
+			Expect(err).NotTo(HaveOccurred(), "Failed to list CSI driver pods across all namespaces")
+
+			// Check that at least one pod exists
+			Expect(pods.Items).NotTo(BeEmpty(), "No CSI driver pods found in any namespace")
+
+			// Check that all pods are running
+			foundRunning := false
+			for _, pod := range pods.Items {
+				if pod.Status.Phase == corev1.PodRunning {
+					foundRunning = true
+					GinkgoWriter.Write([]byte("Found running pod: " + pod.Name + " in namespace " + pod.Namespace + "\n"))
+				}
+			}
+			Expect(foundRunning).To(BeTrue(), "No CSI driver pods are in Running state")
 		})
 	})
 
@@ -123,12 +111,12 @@ var _ = Describe("Scality S3 CSI Driver", func() {
 
 			// Example of creating a PVC
 			// pvc := &corev1.PersistentVolumeClaim{...}
-			// _, err := clientset.CoreV1().PersistentVolumeClaims(testNamespace).Create(context.Background(), pvc, metav1.CreateOptions{})
+			// _, err := clientset.CoreV1().PersistentVolumeClaims("default").Create(context.Background(), pvc, metav1.CreateOptions{})
 			// Expect(err).NotTo(HaveOccurred(), "Failed to create PVC")
 
 			// Wait for the PVC to be bound
 			// Eventually(func() bool {
-			//   pvc, err := clientset.CoreV1().PersistentVolumeClaims(testNamespace).Get(context.Background(), pvcName, metav1.GetOptions{})
+			//   pvc, err := clientset.CoreV1().PersistentVolumeClaims("default").Get(context.Background(), pvcName, metav1.GetOptions{})
 			//   if err != nil {
 			//     return false
 			//   }
