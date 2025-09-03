ce02ea7063bc7fb20dd2386fe34a0f227534200f
Anurag Mittal <1321012+anurag4DSB@users.noreply.github.com>
2025-09-03 12:58:19 +0200

    Enable pod mounter mode by default for SELinux support
    - Change default mounter to pod-based instead of systemd
    - Update Helm charts to use podMounter.enabled instead of experimental.podMounter
    - Add priority classes for headroom management (mount-s3-preempting, mount-s3-headroom)
    - Configure headroom image for pause containers (registry.k8s.io/pause:3.10)
    - Update UsePodMounter() to default to true for SELinux and ROSA support
    - Users can still opt-out by setting podMounter.enabled=false or MOUNTER_KIND=systemd
    - Pod mounter provides better compatibility with Red Hat OpenShift and SELinux contexts

--- pkg/util/env.go
+++ pkg/util/env.go
@@ -2,6 +2,14 @@ package util
 
 import "os"
 
+// UsePodMounter returns true if pod-based mounting should be used.
+// Defaults to true (pod mounter) for SELinux and Red Hat OpenShift support.
+// Set MOUNTER_KIND=systemd to use the legacy systemd mounter.
 func UsePodMounter() bool {
-	return os.Getenv("MOUNTER_KIND") == "pod"
+	mounterKind := os.Getenv("MOUNTER_KIND")
+	if mounterKind == "" {
+		// Default to pod mounter for SELinux support
+		return true
+	}
+	return mounterKind == "pod"
 }
